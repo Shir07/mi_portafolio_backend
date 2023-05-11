775766ba1e1d851436ec93be27f8878b970232a6
Shir07 <priscila.ohannecian7@gmail.com>
2023-05-10 23:04:39 -0300

    Se agregan archivos

---
+++ src/test/java/com/portfolioback/backportfolio/BackportfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolioback.backportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackportfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
